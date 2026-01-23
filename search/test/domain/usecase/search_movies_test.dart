2576978f7f326f9ab2f21985c6a2a41cdc336b30
Haryo Pramanto <gitarringsek@gmail.com>
2026-01-24 01:58:53 +0700

    fix search movies unit test

--- search/test/domain/usecase/search_movies_test.dart
+++ search/test/domain/usecase/search_movies_test.dart
@@ -1,11 +1,14 @@
-import 'package:dartz/dartz.dart';
 import 'package:core/domain/entities/movie/movie.dart';
-import '../../../../search/lib/domain/usecases/search_movies.dart';
+import 'package:core/domain/repositories/movie_repository.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:search/domain/usecases/search_movies.dart';
 
-import '../../../../core/test/helpers/test_helper.mocks.dart';
+import 'search_movies_test.mocks.dart';
 
+@GenerateMocks([MovieRepository])
 void main() {
   late SearchMovies usecase;
   late MockMovieRepository mockMovieRepository;
